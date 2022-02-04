163894c5c821f867e8c5d105f2405c1f938c7ed7
SHUBHANSHU-creator <shubhanshu.kumar2020@vitbhopal.ac.in>
2022-02-04 21:36:04 +0530

    update app.js

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ app.post('/register',function(req,res){
 */
 app.post('/register',function(req,res){
     if (req.body.password == req.body.password1){
+        //checking if the email is already registered
         User.findOne({email:req.body.email},function(err,founduser){
             if(err){
                 console.log(err);
@@ -72,22 +73,37 @@ app.post('/register',function(req,res){
                 if(founduser){
                     res.send('email already exists')
                 }else{
-                    const newuser=new User({
-                        Firstname:req.body.fname,
-                        Lastname:req.body.lname,
-                        adress:req.body.adress,
-                        phonenum:req.body.phnum,
-                        email:req.body.email,
-                        password:req.body.password
-                    })
-                
-                    newuser.save(function(err){
+                    User.findOne({phonenum:req.body.phnum},function(err,foundnum){
                         if(err){
                             console.log(err)
                         }else{
-                            res.render('login')
+                            if(foundnum){//if already number is registered 
+                                res.send('phone number already registered, please try using new number or login using previous one')
+                            }
+                            else{
+                                //If phone number and email are not registered then create a new id for user 
+                                //adding all the detais to the database
+                                const newuser=new User({
+                                    Firstname:req.body.fname,
+                                    Lastname:req.body.lname,
+                                    adress:req.body.adress,
+                                    phonenum:req.body.phnum,
+                                    email:req.body.email,
+                                    password:req.body.password
+                                })
+                            
+                                newuser.save(function(err){
+                                    if(err){
+                                        console.log(err)
+                                    }else{
+                                        res.render('login')
+                                    }
+                                })
+                            }
                         }
                     })
+
+                    
                 }
             }
         })
